4390717064fd55a0769aec05b9cc4039bbf4a56d
Jinlong Ru <jinlongru@gmail.com>
2023-03-04 19:34:29 +0100

    clean code

--- scripts/scripts.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-def get_page_title(client, page_id, column="exp_sample_id"):
-    """
-    from notion_client import Client
-    client = Client(auth=notion_token)
-    """
-    rst = client.pages.retrieve(page_id)
-    return(rst['properties'][column]['title'][0]['plain_text'])
